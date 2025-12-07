f9528e474379bc886327fcbf796eea9820e677ec
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-12-07 13:56:11 +0530

    feat: Add Convex backend infrastructure and integrate it into web and widget applications

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,21 @@
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+
+import { api } from "@workspace/backend/_generated/api";
+
 import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web/app/page.tsx</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(1, 2)}</p>
+    <div className="flex flex-col justify-center items-center min-h-svh">
+      <p>apps/web/app/page.tsx</p>
+      <Button onClick={() => addUser()}>Add User</Button>
+      <div className="mx-auto w-full max-w-sm">
+        <pre>{JSON.stringify(users, null, 2)}</pre>
       </div>
     </div>
   );
--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,9 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import onlyWarn from "eslint-plugin-only-warn"
-import turboPlugin from "eslint-plugin-turbo"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -29,4 +30,42 @@ export const config = [
   {
     ignores: ["dist/**"],
   },
-]
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // Frameworks
+            ["^react", "^next"],
+            // Third-Party Libraries
+            ["^[a-z]", "^@\\w"],
+            // Internal Aliases (General)
+            ["^@/"],
+            // Specific for workspace backend
+            ["^@workspace/backend"],
+            // Specific for workspace ui
+            ["^@workspace/ui"],
+            // Specific Internal Feature Folders
+            ["^@/lib"],
+            ["^@/components", "^@/modules"],
+            ["^@/utils", "^@/helpers"],
+            ["^@/hooks"],
+            // Parent Imports
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            // Sibling Imports
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            // Side Effect Imports
+            ["^\\u0000"],
+            // Catch-All
+            ["^"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+  },
+];
