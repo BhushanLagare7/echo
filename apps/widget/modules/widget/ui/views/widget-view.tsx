27b680b84c9f2103627353adc5569608c65257de
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-12-09 23:53:53 +0530

    feat: Implement Jotai for dynamic widget screen management

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-// import { WidgetFooter } from "../components/widget-footer";
+import { useAtomValue } from "jotai";
+
+import { screenAtom } from "../../atoms/widget-atoms";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface WidgetViewProps {
@@ -8,11 +10,23 @@ interface WidgetViewProps {
 }
 
 export const WidgetView = ({ organizationId }: WidgetViewProps) => {
+  const screen = useAtomValue(screenAtom);
+
+  const screenComponents = {
+    error: <p>TODO: Error screen</p>,
+    loading: <p>TODO: Loading screen</p>,
+    selection: <p>TODO: Selection screen</p>,
+    voice: <p>TODO: Voice screen</p>,
+    auth: <WidgetAuthScreen />,
+    inbox: <p>TODO: Inbox screen</p>,
+    chat: <p>TODO: Chat screen</p>,
+    contact: <p>TODO: Contact screen</p>,
+  };
+
   return (
-    // TODO: Confirm whether we need "min-h-screen" and "min-w-screen" or not
+    // TODO: Confirm whether we need "m in-h-screen" and "min-w-screen" or not
     <main className="flex overflow-hidden flex-col w-full h-full min-h-screen rounded-xl border min-w-screen bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+      {screenComponents[screen]}
     </main>
   );
 };
