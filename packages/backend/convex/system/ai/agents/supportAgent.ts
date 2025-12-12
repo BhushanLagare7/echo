64c179e645a9d2b8e52344b2fd2227087375642a
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-12-13 01:41:04 +0530

    feat: Implement conversation status management, AI response enhancement, infinite scroll, and new button/hint UI components

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -5,6 +5,10 @@ import { components } from "../../../_generated/api";
 
 export const supportAgent = new Agent(components.agent, {
   name: "Support Agent",
-  languageModel: google.chat("gemini-2.5-flash"),
-  instructions: "You are a customer support agent",
+  languageModel: google.chat("gemini-2.5-flash-lite"),
+  instructions: `You are a customer support agent. 
+  You can use the following tools:
+  - resolveConversation: When user expresses finalization of a conversation.
+  - escalateConversation: When user expresses frustration or requests a human explicitly
+`,
 });
